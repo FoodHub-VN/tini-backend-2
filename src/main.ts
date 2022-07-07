bc7b59066b6bc3e368183f6ffaeef52ea779a06a
huynhnhatlong <huynhnhatlong9@gmail.com>
2022-07-07 09:20:50 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import fs from 'fs';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const httpsOptions = {
+    key: fs.readFileSync('./ssl/0000_key-certbot.pem'),
+    cert: fs.readFileSync('./ssl/0000_csr-certbot.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {cors: true, httpsOptions});
   app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}));
   // app.useStaticAssets(join(__dirname, "..", "upload"));
   // add swagger module
