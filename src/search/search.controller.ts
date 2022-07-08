117bf1561fcbc01acd87c5ab815b35c2bfa3bff9
phunht <phu.nguyenpfoem@hcmut.edu.vn>
2022-07-09 02:35:22 +0700

    add search

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,13 +1,32 @@
-import {Controller, Post} from '@nestjs/common';
+import {Body, Controller, Get, Post} from '@nestjs/common';
 import {SearchService} from "./search.service";
+import {SearchUserByNameDto} from "./dto/search-user-by-name.dto";
+import {SearchUserByIdDto} from "./dto/search-user-by-id.dto";
+import {User} from "../database/model/user.model";
+import {SearchPostByKeywordsDto} from "./dto/search-post-by-keywords.dto";
 
 @Controller('search')
 export class SearchController {
     constructor(private readonly searchService:SearchService) {
     }
 
-    @Post('test')
-    test(){
+    @Post('/user/by-name')
+    async searchUserByName(@Body() req: SearchUserByNameDto): Promise<User[]>{
+        return await this.searchService.fetchManyUsersWithName(req.name, req.limit || 5);
+    }
+
+    @Post('/user/by-id')
+    async searchUserById(@Body() req: SearchUserByIdDto): Promise<User>{
+        return await this.searchService.fetchUserWithUsername(req.id);
+    }
+
+    @Post('/post/by-keyword')
+    async searchPostByKeyword(@Body() req: SearchPostByKeywordsDto) {
+        return await this.searchService.fetchBestPostsContainingKeywords(req.keywords, req.limit || 5);
+    }
+
+    @Post('/vendor/by-lat-lng')
+    async searchVendorByLatLng(){
         return "hello";
     }
 }
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
+import {POST_MODEL, USER_MODEL} from "../database/database.constants";
+import {Post, PostModel} from "../database/model/post.model";
+import {User, UserModel} from "../database/model/user.model";
+
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+    constructor(@Inject(USER_MODEL) private userModel: UserModel,
+                @Inject(POST_MODEL) private postModel: PostModel
+    ) {
+    }
+
+    async fetchManyUsersWithName(name: string, limit: number): Promise<User[]> {
+        return this.userModel.find({name}, {_id: 0}).limit(limit).exec();
+    }
+
+    async fetchUserWithUsername(id: string): Promise<User> {
+        return this.userModel.findOne({id}, {_id: 0}).exec();
+    }
+
+    async fetchBestPostsContainingKeywords(keywords: string, limit: number): Promise<Post[]> {
+        return this.postModel.find({
+            $text: {$search: keywords},
+        }, {_id: 0}).exec();
+    }
+}
