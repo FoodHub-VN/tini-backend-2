f88b4df907fbf68ae857d6d16001594bd8cc4ec7
phunht <phu.nguyenpfoem@hcmut.edu.vn>
2022-07-09 21:47:09 +0700

    add image to dish

--- src/database/model/dish.model.ts
+++ src/database/model/dish.model.ts
@@ -2,12 +2,14 @@ import { Document, Schema, SchemaTypes } from 'mongoose';
 
 interface Dish extends Document {
   readonly dishName: string;
+  readonly imageUrl: string;
   readonly description: string;
   readonly cost: number;
 }
 
 const DishSchema = new Schema<Dish>({
   dishName: SchemaTypes.String,
+  imageUrl: SchemaTypes.String,
   description: SchemaTypes.String,
   cost: SchemaTypes.Number,
 }, {
