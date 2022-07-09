f13b081c2fc2a160b7cfb0bec17691724a709fe6
huynhnhatlong <huynhnhatlong9@gmail.com>
2022-07-09 08:25:41 +0700

    .

--- dist/main.js
+++ dist/main.js
@@ -6,15 +6,15 @@ const common_1 = require("@nestjs/common");
 const swagger_1 = require("@nestjs/swagger");
 const fs_1 = require("fs");
 async function bootstrap() {
-    const httpsOptions = {
+    const httpsOptions = process.env.MODE != 'DEV' ? {
         key: (0, fs_1.readFileSync)('./ssl/private.key', 'utf8'),
         cert: (0, fs_1.readFileSync)('./ssl/public.crt', 'utf8'),
-    };
+    } : undefined;
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true, httpsOptions });
     app.useGlobalPipes(new common_1.ValidationPipe({ transform: true, whitelist: true }));
     const config = new swagger_1.DocumentBuilder()
-        .setTitle("[FoodHub APIs]")
-        .setDescription("The RESTful APIs from super dev")
+        .setTitle('[FoodHub APIs]')
+        .setDescription('The RESTful APIs from super dev')
         .addBearerAuth()
         .setVersion('1.0')
         .addTag('FoodHub')
--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import {readFileSync} from 'fs'
+import { readFileSync } from 'fs';
 
 async function bootstrap() {
-  const httpsOptions = {
+
+  const httpsOptions = process.env.MODE != 'DEV' ? {
     key: readFileSync('./ssl/private.key', 'utf8'),
     cert: readFileSync('./ssl/public.crt', 'utf8'),
-  };
-  const app = await NestFactory.create(AppModule, {cors: true, httpsOptions});
-  app.useGlobalPipes(new ValidationPipe({transform: true, whitelist: true}));
+  } : undefined;
+
+  const app = await NestFactory.create(AppModule, { cors: true, httpsOptions });
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   // app.useStaticAssets(join(__dirname, "..", "upload"));
   // add swagger module
   const config = new DocumentBuilder()
-    .setTitle("[FoodHub APIs]")
-    .setDescription("The RESTful APIs from super dev")
+    .setTitle('[FoodHub APIs]')
+    .setDescription('The RESTful APIs from super dev')
     .addBearerAuth()
-      .setVersion('1.0')
+    .setVersion('1.0')
       .addTag('FoodHub')
       .build();
   const document = SwaggerModule.createDocument(app, config);
